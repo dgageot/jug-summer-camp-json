673e09ccfacd89c17e72de9535b2986d31632af6
David Gageot <david@gageot.net>
2012-09-13 16:42:20 +0200

    Add Unit tests

--- src/test/java/legacy/InnTest.java
+++ src/test/java/legacy/InnTest.java
@@ -2,9 +2,63 @@ package legacy;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.fest.assertions.Assertions.assertThat;
+
 public class InnTest {
+  Inn inn = new Inn();
+
   @Test
-  public void should_write_tests() {
+  public void should_list_items() {
+    assertThat(inn.getItems()).onProperty("name").containsExactly("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(20, 0, 7, 80, 20, 6);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(10, 2, 5, 0, 15, 3);
+  }
+
+  @Test
+  public void should_update_items() {
+    inn.updateQuality();
+
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(19, 1, 6, 80, 21, 5);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
+  }
+
+  @Test
+  public void should_update_items_twice() {
+    inn.updateQuality();
+    inn.updateQuality();
+
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(18, 2, 5, 80, 22, 4);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(8, 0, 3, 0, 13, 1);
+  }
+
+  @Test
+  public void should_update_items_a_lot() {
+    for (int day = 0; day < 50; day++) {
+      inn.updateQuality();
+    }
+
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(0, 50, 0, 80, 0, 0);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(-40, -48, -45, 0, -35, -47);
+  }
+
+  @Test
+  public void should_test_against_legacy_code() {
+    LegacyInn legacyInn = new LegacyInn();
+
+    for (int day = 0; day < 100; day++) {
+      List<Item> items = inn.getItems();
+      List<Item> legacyItems = legacyInn.getItems();
+
+      for (int i = 0; i < legacyItems.size(); i++) {
+        assertThat(items.get(i).getName()).isEqualTo(legacyItems.get(i).getName());
+        assertThat(items.get(i).getQuality()).isEqualTo(legacyItems.get(i).getQuality());
+        assertThat(items.get(i).getSellIn()).isEqualTo(legacyItems.get(i).getSellIn());
+      }
+
+      inn.updateQuality();
+      legacyInn.updateQuality();
+    }
   }
 }
-
