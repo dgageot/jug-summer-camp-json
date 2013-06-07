ba45e30580732dd6f15dfa6b5f465ce3131c5e1d
dgageot <david@gageot.net>
2013-06-07 19:03:37 +0200

    Cloudbees

--- src/main/java/legacy/InnWeb.java
+++ src/main/java/legacy/InnWeb.java
@@ -3,12 +3,14 @@ package legacy;
 import java.io.*;
 import java.net.*;
 
-import com.google.common.base.*;
+import com.google.common.io.*;
+import com.google.gson.*;
 import com.sun.net.httpserver.*;
 
 public class InnWeb {
-	public static void main(String[] args) throws Exception {
-		int port = Integer.parseInt(Objects.firstNonNull(System.getenv("PORT"), "8080"));
+	public static void main(String[] args) throws IOException {
+		int port = Integer.parseInt(System.getProperty("app.port", "8080"));
+		System.out.println(port);
 
 		HttpServer
 				.create(new InetSocketAddress(port), 0)
@@ -17,10 +19,14 @@ public class InnWeb {
 					public void handle(HttpExchange exchange) throws IOException {
 						String uri = exchange.getRequestURI().toString();
 
-						if ("/".equals(uri)) {
-							String body = "Hello World";
-							byte[] response = body.getBytes();
-							exchange.getResponseHeaders().add("Content-Type", "text/plain");
+						if ("/items".equals(uri)) {
+							byte[] response = new Gson().toJson(new Inn().getItems()).getBytes();
+							exchange.getResponseHeaders().add("Content-Type", "application/json");
+							exchange.sendResponseHeaders(200, response.length);
+							exchange.getResponseBody().write(response);
+						} else if ("/".equals(uri)) {
+							byte[] response = Files.toByteArray(new File("app/index.html"));
+							exchange.getResponseHeaders().add("Content-Type", "text/html");
 							exchange.sendResponseHeaders(200, response.length);
 							exchange.getResponseBody().write(response);
 						} else {
