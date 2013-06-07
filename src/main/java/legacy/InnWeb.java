44b51d67abfe73d6ef91898dca33cf61c49bde1b
dgageot <david@gageot.net>
2013-06-07 17:04:35 +0200

    wip

--- src/main/java/legacy/InnWeb.java
+++ src/main/java/legacy/InnWeb.java
@@ -1,30 +1,36 @@
 package legacy;
 
+import java.io.*;
 import java.net.*;
 
+import com.google.common.base.*;
 import com.sun.net.httpserver.*;
 
 public class InnWeb {
 	public static void main(String[] args) throws Exception {
-		int port = Integer.parseInt(System.getenv().getOrDefault("PORT", "8080"));
+		int port = Integer.parseInt(Objects.firstNonNull(System.getenv("PORT"), "8080"));
 
-		HttpServer httpServer = HttpServer.create(new InetSocketAddress(port), 0);
+		HttpServer
+				.create(new InetSocketAddress(port), 0)
+				.createContext("/", new HttpHandler() {
+					@Override
+					public void handle(HttpExchange exchange) throws IOException {
+						String uri = exchange.getRequestURI().toString();
 
-		httpServer.createContext("/", exchange -> {
-			String uri = exchange.getRequestURI().toString();
+						if ("/".equals(uri)) {
+							String body = "Hello World";
+							byte[] response = body.getBytes();
+							exchange.getResponseHeaders().add("Content-Type", "text/plain");
+							exchange.sendResponseHeaders(200, response.length);
+							exchange.getResponseBody().write(response);
+						} else {
+							exchange.sendResponseHeaders(404, 0);
+						}
 
-			if ("/".equals(uri)) {
-				byte[] response = "Hello World".getBytes();
-				exchange.getResponseHeaders().add("Content-Type", "text/plain");
-				exchange.sendResponseHeaders(200, response.length);
-				exchange.getResponseBody().write(response);
-			} else {
-				exchange.sendResponseHeaders(404, 0);
-			}
-
-			exchange.close();
-		});
-
-		httpServer.start();
+						exchange.close();
+					}
+				})
+				.getServer()
+				.start();
 	}
 }
