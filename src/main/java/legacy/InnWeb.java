fdac4273cc345ac14a089cc25d33043bf4491db2
dgageot <david@gageot.net>
2013-06-08 14:24:52 +0200

    wip

--- src/main/java/legacy/InnWeb.java
+++ src/main/java/legacy/InnWeb.java
@@ -1,44 +1,37 @@
 package legacy;
 
-import java.io.*;
 import java.net.*;
-import java.nio.file.*;
 
-import com.google.gson.*;
 import com.sun.net.httpserver.*;
 
 public class InnWeb {
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		int port = Integer.parseInt(System.getProperty("app.port", "8080"));
-		System.out.println(port);
 
-		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
-		server.createContext("/", (HttpExchange exchange) -> {
-			byte[] response = null;
+		HttpServer.
+				create(new InetSocketAddress(port), 0).
+				createContext("/", (HttpExchange exchange) -> {
+					byte[] body = null;
 
-			String uri = exchange.getRequestURI().toString();
-			switch (uri) {
-				case "/": {
-					response = Files.readAllBytes(Paths.get("app", "index.html"));
-					exchange.getResponseHeaders().add("Content-Type", "text/html");
-					break;
-				}
-				case "/items": {
-					String body = new Gson().toJson(new Inn().getItems());
-					response = body.getBytes();
-					exchange.getResponseHeaders().add("Content-Type", "application/json");
-					break;
-				}
-			}
+					String uri = exchange.getRequestURI().toString();
+					switch (uri) {
+						case "/": {
+							body = "Hello World".getBytes();
+							exchange.getResponseHeaders().add("Content-Type", "text/html");
+							break;
+						}
+					}
 
-			if (response != null) {
-				exchange.sendResponseHeaders(200, response.length);
-				exchange.getResponseBody().write(response);
-			} else {
-				exchange.sendResponseHeaders(404, 0);
-			}
-			exchange.close();
-		});
-		server.start();
+					if (body != null) {
+						exchange.sendResponseHeaders(200, body.length);
+						exchange.getResponseBody().write(body);
+					} else {
+						exchange.sendResponseHeaders(404, 0);
+					}
+					exchange.close();
+				}).
+				getServer().
+				start();
+
 	}
 }
