7b7ccb43103d15fc84126759b134d1eb8db9caef
dgageot <david@gageot.net>
2013-06-08 14:12:33 +0200

    wip

--- src/main/java/legacy/InnWeb.java
+++ src/main/java/legacy/InnWeb.java
@@ -12,37 +12,33 @@ public class InnWeb {
 		int port = Integer.parseInt(System.getProperty("app.port", "8080"));
 		System.out.println(port);
 
-		HttpServer
-				.create(new InetSocketAddress(port), 0)
-				.createContext("/", new HttpHandler() {
-					@Override
-					public void handle(HttpExchange exchange) throws IOException {
-						String uri = exchange.getRequestURI().toString();
+		HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+		server.createContext("/", (HttpExchange exchange) -> {
+			byte[] response = null;
 
-						switch (uri) {
-							case "/items": {
-								byte[] response = new Gson().toJson(new Inn().getItems()).getBytes();
-								exchange.getResponseHeaders().add("Content-Type", "application/json");
-								exchange.sendResponseHeaders(200, response.length);
-								exchange.getResponseBody().write(response);
-								break;
-							}
-							case "/": {
-								byte[] response = Files.readAllBytes(Paths.get("app", "index.html"));
-								exchange.getResponseHeaders().add("Content-Type", "text/html");
-								exchange.sendResponseHeaders(200, response.length);
-								exchange.getResponseBody().write(response);
-								break;
-							}
-							default:
-								exchange.sendResponseHeaders(404, 0);
-								break;
-						}
+			String uri = exchange.getRequestURI().toString();
+			switch (uri) {
+				case "/": {
+					response = Files.readAllBytes(Paths.get("app", "index.html"));
+					exchange.getResponseHeaders().add("Content-Type", "text/html");
+					break;
+				}
+				case "/items": {
+					String body = new Gson().toJson(new Inn().getItems());
+					response = body.getBytes();
+					exchange.getResponseHeaders().add("Content-Type", "application/json");
+					break;
+				}
+			}
 
-						exchange.close();
-					}
-				})
-				.getServer()
-				.start();
+			if (response != null) {
+				exchange.sendResponseHeaders(200, response.length);
+				exchange.getResponseBody().write(response);
+			} else {
+				exchange.sendResponseHeaders(404, 0);
+			}
+			exchange.close();
+		});
+		server.start();
 	}
 }
