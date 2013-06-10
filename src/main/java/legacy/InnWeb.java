004808228ee5d48097e89f0bfd7efc9332e053dd
dgageot <david@gageot.net>
2013-06-10 17:22:34 +0200

    TEST

--- src/main/java/legacy/InnWeb.java
+++ src/main/java/legacy/InnWeb.java
@@ -5,33 +5,30 @@ import java.net.*;
 import com.sun.net.httpserver.*;
 
 public class InnWeb {
-	public static void main(String[] args) throws Exception {
-		int port = Integer.parseInt(System.getProperty("app.port", "8080"));
+  public static void main(String[] args) throws Exception {
+    int port = Integer.parseInt(System.getProperty("app.port", "8080"));
 
-		HttpServer.
-				create(new InetSocketAddress(port), 0).
-				createContext("/", (HttpExchange exchange) -> {
-					byte[] body = null;
 
-					String uri = exchange.getRequestURI().toString();
-					switch (uri) {
-						case "/": {
-							body = "Hello World".getBytes();
-							exchange.getResponseHeaders().add("Content-Type", "text/html");
-							break;
-						}
-					}
+    HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+    server.createContext("/", (HttpExchange exchange) -> {
+      String uri = exchange.getRequestURI().toString();
 
-					if (body != null) {
-						exchange.sendResponseHeaders(200, body.length);
-						exchange.getResponseBody().write(body);
-					} else {
-						exchange.sendResponseHeaders(404, 0);
-					}
-					exchange.close();
-				}).
-				getServer().
-				start();
+      switch (uri) {
+        case "/": {
+          byte[] body = "Hello World".getBytes();
+          exchange.getResponseHeaders().add("Content-Type", "text/html");
+          exchange.sendResponseHeaders(200, body.length);
+          exchange.getResponseBody().write(body);
+          break;
+        }
+        default: {
+          exchange.sendResponseHeaders(404, 0);
+          break;
+        }
+      }
 
-	}
+      exchange.close();
+    });
+    server.start();
+  }
 }
--- src/test/java/legacy/InnTest.java
+++ src/test/java/legacy/InnTest.java
@@ -1,10 +1,25 @@
 package legacy;
 
-import org.junit.Test;
+import static org.fest.assertions.Assertions.*;
+
+import org.junit.*;
 
 public class InnTest {
+  Inn inn = new Inn();
+
   @Test
   public void should_write_tests() {
+    assertThat(inn.getItems()).onProperty("name").containsExactly("+5 Dexterity Vest", "Aged Brie", "Elixir of the Mongoose", "Sulfuras, Hand of Ragnaros", "Backstage passes to a TAFKAL80ETC concert", "Conjured Mana Cake");
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(20, 0, 7, 80, 20, 6);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(10, 2, 5, 0, 15, 3);
+  }
+
+  @Test
+  public void should_write_tests2() {
+    inn.updateQuality();
+
+    assertThat(inn.getItems()).onProperty("quality").containsExactly(19, 1, 6, 80, 21, 5);
+    assertThat(inn.getItems()).onProperty("sellIn").containsExactly(9, 1, 4, 0, 14, 2);
   }
 }
 
