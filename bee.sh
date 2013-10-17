06758657d21fc5462540cf62cb164bc5e182bcf4
David Gageot <david@gageot.net>
2013-10-17 10:18:37 +0200

    REBOOT

--- bee.sh
+++ bee.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-mvn clean install && bees app:deploy -a retour1024/usi -t java -R class=legacy.InnWeb -R java_version=1.8 -R classpath='classes:lib/*' target/distribution.zip
+mvn clean install && bees app:deploy \
+ -a retour1024/jug \
+ -t java \
+ -R class=legacy.InnWeb \
+ -R java_version=1.8 \
+ -R JAVA_OPTS="-Dhttp.disable.classpath=true -DPROD_MODE=true" \
+ -R classpath='classes:lib/*' target/distribution.zip
